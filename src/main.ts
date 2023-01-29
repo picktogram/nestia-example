88b827648900d48db924e93cd68b2b80c9af216c
Author kakasoo <kscodebase@gmail.com>
2023-01-29 15:27:47 +0900

    feat : validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerSetting } from './config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+      transform: true,
+      whitelist: true,
+    }),
+  );
 
   app.enableCors();
 
