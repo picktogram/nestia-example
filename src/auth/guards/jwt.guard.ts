491ea985bdff69e731ecc630cea9ad4d91ff7d17
Author kakasoo <kscodebase@gmail.com>
2023-01-29 14:41:17 +0900

    feat : 인증 정보가 없어서 발생하는 에러에 대한 커스텀 처리

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,5 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ERROR } from '@root/config/constant/error';
+import { DecodedUserToken } from '@root/types';
 
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {}
+export class JwtGuard extends AuthGuard('jwt') {
+  handleRequest<TUser = DecodedUserToken>(
+    err: any,
+    user: any,
+    info: any,
+    context: ExecutionContext,
+    status?: any,
+  ): TUser {
+    if (info.message === 'No auth token') {
+      throw new BadRequestException(ERROR.NO_AUTH_TOKEN);
+    }
+    return super.handleRequest(err, user, info, context, status);
+  }
+}
--- src/config/constant/error.ts
+++ src/config/constant/error.ts
@@ -1,3 +1,4 @@
 export const ERROR = {
   ALREADY_CREATED_EMAIL: { code: 4001, message: '이미 생성된 이메일입니다.' },
+  NO_AUTH_TOKEN: { code: 4002, message: '인증이 필요합니다.' },
 } as const;
