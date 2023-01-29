cddc901e0ce24b0afcaaaf4d808585afae66a734
Author kakasoo <kscodebase@gmail.com>
2023-01-29 12:37:49 +0900

    feat : 슬랙에 에러 메시지가 쌓이도록 하는 기능을 추가

--- src/common/filters/http-exception.fiter.ts
+++ src/common/filters/http-exception.fiter.ts
@@ -4,12 +4,15 @@ import {
   ArgumentsHost,
   HttpException,
 } from '@nestjs/common';
+import { SlackService } from '@root/external/slack/slack.service';
 import { Request, Response } from 'express';
 
 const UNCHATCHED_ERROR = '서버에서 캐치되지 못한 에러입니다.';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(private readonly slackService: SlackService) {}
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
@@ -24,6 +27,22 @@ export class HttpExceptionFilter implements ExceptionFilter {
       `error\n${request.method} ${path} ${requestToResponse}\n` +
         `currentTime : ${new Date()}]\n`,
     );
+
+    if (!code) {
+      let slackMessageForm = `error\n`;
+      slackMessageForm += `${request.method} ${path} ${exception}\n`;
+      slackMessageForm += `user : ${JSON.stringify(user)}\n`;
+      slackMessageForm += `query : ${JSON.stringify(query)}\n`;
+      slackMessageForm += `date : ${new Date()}\n`;
+
+      slackMessageForm += `errorMessage: ${message}\n`;
+      slackMessageForm += `errorStack: 아래 메시지 참고\n`;
+      exception.stack.split('\n').forEach((stackMessage) => {
+        slackMessageForm += `\t${stackMessage}\n`;
+      });
+
+      this.slackService.sendToServerErrorChannel(slackMessageForm);
+    }
 
     response.status(status).json({
       statusCode: status,
