2d41af7856f8f60fcd4630aaad1c5a3c5988c2b3
kakasoo <kscodebase@gmail.com>
2023-02-03 00:54:36 +0900

    chore : 미완성이지만, 일단 에러 스키마 작성을 돕는 함수의 기본 구조를 잡는다

--- src/config/constant/error.ts
+++ src/config/constant/error.ts
@@ -6,6 +6,8 @@ export const ERROR = {
   IS_SAME_POSITION: { code: 4003, message: '이미지의 정렬 값이 동일한 경우가 존재합니다.' },
   CANNOT_FINDONE_ARTICLE: { code: 4004, message: '게시글을 찾지 못했습니다.' },
   SELECT_MORE_THAN_ONE_BODY_IMAGE: { code: 4005, message: '적어도 1장 이상의 이미지를 골라야 합니다.' },
+  NOT_FOUND_ARTICLE_TO_COMMENT: { code: 4006, message: '댓글을 작성할 게시글을 찾지 못했습니다.' },
+  TOO_MANY_REPORTED_ARTICLE: { code: 4007, message: '신고가 접수된 게시글이라 댓글 작성이 불가능합니다.' },
 } as const;
 
 type KeyOfError = keyof typeof ERROR;
@@ -17,6 +19,40 @@ export const createErrorSchema = (error: ValueOfError): SchemaObject => {
     properties: {
       code: { type: 'number', example: error.code },
       message: { type: 'string', example: error.message },
+    },
+  };
+};
+
+/**
+ * TODO :
+ */
+
+type Push<T extends any[], U> = [...T, U];
+type NTuple<N extends number, T extends any[] = []> = T['length'] extends N ? T : NTuple<N, Push<T, any>>;
+
+export const createErrorSchemas = <T extends string[], U extends T['length']>(
+  description: T,
+  error: NTuple<U, ValueOfError[]>,
+): SchemaObject => {
+  return {
+    type: 'array',
+    items: {
+      anyOf: [
+        {
+          description: description[0],
+          properties: {
+            code: { type: 'number', example: error[0].code },
+            message: { type: 'string', example: error[0].message },
+          },
+        },
+        {
+          description: description[1],
+          properties: {
+            code: { type: 'number', example: error[1].code },
+            message: { type: 'string', example: error[1].message },
+          },
+        },
+      ],
     },
   };
 };
