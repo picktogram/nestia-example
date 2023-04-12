649d48af4912d4cc479ff265a4389e2d0f3d8f56
kakasoo <kscodebase@gmail.com>
2023-04-12 19:39:51 +0900

    fix : 생성자 수정으로 인한 오류 수정

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.fiter';
 import { SwaggerSetting } from './config/swagger';
-import { SlackService } from './external/slack/slack.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,7 +16,7 @@ async function bootstrap() {
   );
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.useGlobalFilters(new HttpExceptionFilter(app.get(SlackService)));
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   app.enableCors();
 
