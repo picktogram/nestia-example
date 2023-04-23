7769224e1696f0632203de53280ac8966e3a9f53
kakasoo <kscodebase@gmail.com>
2023-04-23 16:06:52 +0900

    feat : compression 적용

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.fiter';
 import { SwaggerSetting } from './config/swagger';
+import compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,7 +18,7 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalFilters(new HttpExceptionFilter());
-
+  app.use(compression());
   app.enableCors();
 
   SwaggerSetting(app);
